5ab6d346cd1ef583f8a9afda0e5a2b4d7fb8cdc2
BuseDev <buseboylan48@gmail.com>
2023-04-14 18:44:28 +0300

    commit

---
+++ javascript_beginner/loop_while.js
@@ -0,0 +1,25 @@
+//while
+
+let i = 0;
+let text = "";
+
+while(i < 10){
+    text += "The number is " + i + "\n";
+    i++;
+}
+
+console.log(text);
+
+
+//do while
+
+let j = 0;
+let txt = "";
+
+do{
+    txt += "The number is " + j + "\n";
+    j++;
+}while(j < 10);
+
+console.log(txt);
+
